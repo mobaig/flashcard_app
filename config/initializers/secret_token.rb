a09d623bb5e3ef0b02f632aa86c3bf6338972a63
Mo Baig <mohammed.m.baig@gmail.com>
2012-03-11 22:38:36 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashcardApp::Application.config.secret_token = 'd27532070bdce5d6040df30bee163e4d6591da3a01b73a2d3f02ba62bed077d6591d75d8a4337b3862326c4e0457b7a4e1e858bfb3832d143c04be82edd35482'
