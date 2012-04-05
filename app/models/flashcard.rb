ca9d9c0b645df0c00dd8fced6dd936b533aada98
Mo Baig <mobaig@notonthehighstreet.com>
2012-04-05 22:03:36 +0100

    Ensure no cards with the same English meaning are created twice.

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -3,10 +3,15 @@ class Flashcard < ActiveRecord::Base
   def self.build_deck
     card_data = XMLCardParser.retrieve_card_data
     card_data.each do |c|
-      Flashcard.create( :english => c.english,
-                        :pinyin => c.pinyin,
-                        :character => c.character,
-                        :category => c.category)
+      exists = Flashcard.find_by_english(c.english)
+      if !exists
+        Flashcard.create( 
+          :english => c.english,
+          :pinyin => c.pinyin,
+          :character => c.character,
+          :category => c.category)
+      end
     end
   end
+  
 end
